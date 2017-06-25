973d307f828fb64025739aaf1fb53b5e2a5e0ffb
Mario Härtwig <mario.haertwig@gmail.com>
2017-06-25 13:31:07 +0200

    Add header, created demo and API pages.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="header">
+      <nav>
+        <div id="brand">OpenLayers Editor</div>
+        <a href="#/api">API</a>
+        <a href="https://github.com/geops/ole2">Code</a>
+        <a href="http://geops.de/blog"></a>
+      </nav>
+    </div>
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +19,47 @@ export default {
 </script>
 
 <style>
+body {
+  font-family: Ubuntu, sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  margin: 0;
+}
+
+a, a:visited {
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#header {
+  border-bottom: 2px solid #61849c;
+  color: #61849c;
+  height: 20px;
+  padding: 20px;
+  text-align: right;
+}
+
+#header a, #header a:visited {
+  color: #61849c;
+}
+
+#brand {
+  font-size: 20px;
+  font-weight: bold;
+  height: 60px;
+  line-height: 60px;
+  left: 10px;
+  position: absolute;
+  top: 0;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import Demo from '@/components/Demo'
+import API from '@/components/API'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'Demo',
+      component: Demo
+    },
+    {
+      path: '/',
+      name: 'API',
+      component: API
     }
   ]
 })
